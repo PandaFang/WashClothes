35459457f14ff0d26f705b544a98e7b04f77232c
Panda Fang <f-ghost@qq.com>
2019-01-14 21:40:22 +0800

    封装网络请求 增加一个login接口演示代码

--- src/api/AppService.js
+++ src/api/AppService.js
@@ -4,6 +4,11 @@ class AppService {
     sendCode = () => {
         return request('/today', 'get');
     }
+
+    login=(username, password) => {
+        let param = {username:username, password:password}
+        return request('/login', 'post', param)
+    }
 }
 
 export default  new AppService()
