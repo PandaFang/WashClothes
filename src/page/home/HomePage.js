0d5b7ffa6740e337e18c9792e3283a70afed1a43
Panda Fang <f-ghost@qq.com>
2019-01-15 22:56:18 +0800

    增加“再按一次退出”功能

--- src/page/home/HomePage.js
+++ src/page/home/HomePage.js
@@ -4,6 +4,9 @@ import {
     View,
     Image,
     Dimensions, StatusBar,
+    BackHandler,
+    Platform,
+    ToastAndroid
 } from "react-native";
 import Banner from './Banner'
 import Service from './Service'
@@ -13,6 +16,30 @@ import Constant from '../../Constant'
 const {width, height} = Dimensions.get('window')
 // 首页
 class HomePage extends Component {
+
+    componentWillMount() {
+        if (Platform.OS === 'android') {
+            BackHandler.addEventListener("hardwareBackPress", this.onBackAndroid);
+        }
+    }
+
+    lastBackPressed = 0;
+    onBackAndroid = () => {
+        if (this.lastBackPressed && this.lastBackPressed + 2000 >= Date.now()) {
+            BackHandler.exitApp();
+            return false
+        }
+        this.lastBackPressed = Date.now();
+        ToastAndroid.show('再按一次退出', ToastAndroid.SHORT);
+        return true
+    }
+
+    componentWillUnmount() {
+        if (Platform.OS === 'android') {
+            BackHandler.removeEventListener("hardwareBackPress", this.onBackAndroid);
+        }
+    }
+
     render() {
       console.log('width:' , width)
       return (
